0746fb5c360f671c94731a51012774acf80ea45a
Ts. Vadim <vadim.ts.yclass@gmail.com>
2021-02-07 22:33:10 +0700

    Article renamed to FileModel

--- web-app/krakenDataAnalyzer/main/models.py
+++ web-app/krakenDataAnalyzer/main/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.conf import settings
+from os import path
 
 
-class Article(models.Model):
+class FileModel(models.Model):
 	title = models.CharField(max_length=250)
 	content = models.TextField(blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
-	photo = models.FileField(upload_to='media/photos/%Y/%m/%d/')
+	photo = models.FileField(upload_to='files/')
